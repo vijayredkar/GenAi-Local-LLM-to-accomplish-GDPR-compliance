fc30a4ab92ce94bcba3ad7e2adbb5de83238f486
Vijay Redkar <vijay0512@yahoo.com>
2024-05-08 00:56:14 +0400

    POST endpoint to test LLM server

--- gen-ai-llm-gdpr/src/main/java/com/genai/llm/privacy/mgt/service/RetrievalService.java
+++ gen-ai-llm-gdpr/src/main/java/com/genai/llm/privacy/mgt/service/RetrievalService.java
@@ -102,4 +102,42 @@ public class RetrievalService
 		System.out.println("---- completed orchestrateLLMServerOnly response : \n"+ response);
 		return response;
 	}
+	
+	//vj5
+		/*
+		 * invoke LLM engine
+		 */
+		public String orchestrateLLMServerOnlyWithBigPayload(String text, boolean testMode, String context) //vj2
+		{	
+			System.out.println("\n---- started orchestrateLLMServerOnlyWithBigPayload");
+			String userPrompt = text;
+			
+			//--step -1  : enhance the user prompt with the context information from the DB 
+			//String contextFromVectorDb = vectorDataSvc.retrieve(contextType, userPrompt); //vj3
+			String contextFromVectorDb = ""; //vj1
+			System.out.println("**** VectorDB de-activated");
+			
+			//String promptWithFullContext = systemMessage + " " + contextFromVectorDb + "  "+  "\"" + userPrompt + "\"";
+			
+			/*
+			String systemMessage1 = "You are an automation testing expert.";
+			String contextFromVectorDb1 = "Your task is to generate test scripts in Cucumber language for the API spec provided below. API spec is: ";
+			String promptWithFullContext = systemMessage1 + " " + contextFromVectorDb1 + "  "+  userPrompt;
+			System.out.println("---- constructed RAG promptWithFullContext \n"+promptWithFullContext);		
+			*/
+			
+			String systemMessage1 = context;
+			String contextFromVectorDb1 = "";			
+			String promptWithFullContext = systemMessage1 + " " + contextFromVectorDb1 + " \n "+  userPrompt;
+			System.out.println("---- constructed RAG promptWithFullContext \n"+promptWithFullContext);	
+			
+			//vj3
+			//--step -2 : invoke the LLM inferencing engine with the fully constructed prompt
+			String response = largeLangModelSvc.generate(promptWithFullContext, testMode);//vj2
+			//String response = contextFromVectorDb;
+			//System.out.println("**** Ollama LLM server de-activated");
+			
+			System.out.println("---- completed orchestrateLLMServerOnlyWithBigPayload response : \n"+ response);
+			return response;
+		}
 }
