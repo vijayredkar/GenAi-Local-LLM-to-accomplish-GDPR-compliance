973be0f04600f622d6bf8d3cd50a66ae6da8a300
Vijay Redkar <vijay0512@yahoo.com>
2024-06-11 11:07:07 +0400

    VM llama3-70b

--- gen-ai-llm-gdpr/src/main/java/com/genai/llm/privacy/mgt/DataPrivacyMgtApplication.java
+++ gen-ai-llm-gdpr/src/main/java/com/genai/llm/privacy/mgt/DataPrivacyMgtApplication.java
@@ -23,6 +23,34 @@ public class DataPrivacyMgtApplication
 	System.out.println("\n******************** Please ensure that your Vector DB istance is running and reachable ********************");
 	System.out.println("\n******************** Please ensure that your Vector DB istance is running and reachable ********************");
 	System.out.println("\n******************** Please ensure that your Vector DB istance is running and reachable ********************");
+      //vj12
+	/*
+	String deploymentEnvPgOcp = "Bawaba-PG-OCP";
+	String deploymentEnvPgVM = "Bawaba-PG-VM";
+	*/
+	List<String> validDeploymentEnvs = new ArrayList<String>();
+	validDeploymentEnvs.add("Bawaba-PG-OCP");
+	validDeploymentEnvs.add("Bawaba-PG-VM");
+	
+	String deploymentEnv = "Bawaba-PG-VM";               //default	
+	if(args.length > 0)
+	{
+		if(!"".equals(args[0].trim()))
+		{
+		 System.out.println("---- args[0] "+args[0]);
+		 deploymentEnv = args[0];
+		}
+		
+		if(!validDeploymentEnvs.contains(deploymentEnv))
+		{
+			System.out.println("**** Error - invalid deployment env. Exiting ....");
+			System.exit(-1);
+		}
+	}	
+	System.out.println("---- deploymentEnv is set to: "+deploymentEnv);
+	System.setProperty("deployment_env", deploymentEnv);		
+	System.out.println("---- System.getProperty(deployment_env): "+System.getProperty("deployment_env"));
+	   
 	SpringApplication.run(DataPrivacyMgtApplication.class, args);
    }  
 }
