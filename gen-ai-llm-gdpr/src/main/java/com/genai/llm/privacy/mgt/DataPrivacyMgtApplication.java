85e7cb10d4c9128aeca5c438cf6d454eb1af7d7b
Vijay Redkar <vijay0512@yahoo.com>
2024-06-29 19:10:39 +0400

    PG multi env

--- gen-ai-llm-gdpr/src/main/java/com/genai/llm/privacy/mgt/DataPrivacyMgtApplication.java
+++ gen-ai-llm-gdpr/src/main/java/com/genai/llm/privacy/mgt/DataPrivacyMgtApplication.java
@@ -20,39 +20,42 @@ import java.util.List;
 @SpringBootApplication
 public class DataPrivacyMgtApplication 
 {	
-  public static void main(String[] args)
-   {
-	System.out.println("\n******************** Please ensure that your Vector DB istance is running and reachable ********************");
-	System.out.println("\n******************** Please ensure that your Vector DB istance is running and reachable ********************");
-	System.out.println("\n******************** Please ensure that your Vector DB istance is running and reachable ********************");
-      //vj12
-	/*
-	String deploymentEnvPgOcp = "Bawaba-PG-OCP";
-	String deploymentEnvPgVM = "Bawaba-PG-VM";
-	*/
-	List<String> validDeploymentEnvs = new ArrayList<String>();
-	validDeploymentEnvs.add("Bawaba-PG-OCP");
-	validDeploymentEnvs.add("Bawaba-PG-VM");
-	
-	String deploymentEnv = "Bawaba-PG-VM";               //default	
-	if(args.length > 0)
+public static void main(String[] args) 
 	{
-		if(!"".equals(args[0].trim()))
-		{
-		 System.out.println("---- args[0] "+args[0]);
-		 deploymentEnv = args[0];
-		}
+		System.out.println("\n******************** Please ensure that your Vector DB istance is running and reachable ********************");
+		System.out.println("\n******************** Please ensure that your Vector DB istance is running and reachable ********************");
+		System.out.println("\n******************** Please ensure that your Vector DB istance is running and reachable ********************");
+	      //vj12
+		/*
+		String deploymentEnvPgOcp = "Bawaba-PG-OCP";
+		String deploymentEnvPgVM = "Bawaba-PG-VM";
+		*/
+		//vj15
+		List<String> validDeploymentEnvs = new ArrayList<String>();
+		validDeploymentEnvs.add("Bawaba-PG-OCP_internal");//connect to PG-OCP with internal OCP svc only exposed endpoints
+		validDeploymentEnvs.add("Bawaba-PG-OCP_external");//connect to PG-OCP with external ENBD machine exposed endpoints
+		validDeploymentEnvs.add("Bawaba-PG-VM_internal"); //connect to PG-VM  with internal container only exposed endpoints
+		validDeploymentEnvs.add("Bawaba-PG-VM_external"); //connect to PG-VM  with external ENBD machine exposed endpoints
 		
-		if(!validDeploymentEnvs.contains(deploymentEnv))
+		String deploymentEnv = "Bawaba-PG-VM_internal";               //default	 
+		if(args.length > 0)
 		{
-			System.out.println("**** Error - invalid deployment env. Exiting ....");
-			System.exit(-1);
-		}
-	}	
-	System.out.println("---- deploymentEnv is set to: "+deploymentEnv);
-	System.setProperty("deployment_env", deploymentEnv);		
-	System.out.println("---- System.getProperty(deployment_env): "+System.getProperty("deployment_env"));
-	   
-	SpringApplication.run(DataPrivacyMgtApplication.class, args);
-   }  
+			if(!"".equals(args[0].trim()))
+			{
+			 System.out.println("---- args[0] "+args[0] + "   args[1] "+args[1]);
+			 deploymentEnv = args[0] + "_"+args[1]; //Bawaba-PG-VM_external     Bawaba-PG-VM_internal
+			}
+			
+			if(!validDeploymentEnvs.contains(deploymentEnv))
+			{
+				System.out.println("**** Error - invalid deployment env. Exiting ....");
+				System.exit(-1);
+			}
+		}	
+		System.out.println("---- deploymentEnv is set to: "+deploymentEnv);
+		System.setProperty("deployment_env", deploymentEnv);		
+		System.out.println("---- System.getProperty(deployment_env): "+System.getProperty("deployment_env"));
+				
+		SpringApplication.run(DataPrivacyMgtApplication.class, args);		
+	}
 }
