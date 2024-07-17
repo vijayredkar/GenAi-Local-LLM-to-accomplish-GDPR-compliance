5d90fda9335631fabfad26f366f2b42a326379fa
Vijay Redkar <vijay0512@yahoo.com>
2024-07-17 17:46:47 +0400

    logs RCA

---
+++ gen-ai-llm-gdpr/src/main/java/com/genai/llm/privacy/mgt/service/TokenizationData.java
@@ -0,0 +1,15 @@
+package com.genai.llm.privacy.mgt.service;
+
+public class TokenizationData 
+{
+	private static final long serialVersionUID = 1L;
+	private String urc;
+	
+	public String getUrc() {
+		return urc;
+	}
+	public void setUrc(String urc) {
+		this.urc = urc;
+	}
+	
+}
